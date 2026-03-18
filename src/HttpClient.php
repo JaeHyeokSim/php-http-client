d0a7b1f64980127bc889ac746c656302f6947280
JaeHyeokSim <01088825238sjh@gmail.com>
2026-03-18 15:25:06 +0900

    Add custom headers support to http client

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -1,27 +1,35 @@
 <?php
 
-class HttpClient {
+class HttpClient
+{
 
 	private $middleware;
 
-	public function __construct() {
+	public function __construct()
+	{
 
 		$this->middleware = new MiddlewareStack();
 	}
 
-	public function addMiddleware(callable $middleware) {
+	public function addMiddleware(callable $middleware)
+	{
 
 		$this->middleware->push($middleware);
 	}
 
-	public function send(Request $request) {
+	public function send(Request $request)
+	{
 
-		$handler = function($request) {
+		$handler = function ($request) {
 
 			$ch = curl_init();
 
 			curl_setopt($ch, CURLOPT_URL, $request->url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, [
+				"User-Agent: Mozilla/5.0",
+				"Accept: text/html"
+			]);
 
 			$body = curl_exec($ch);
 			$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
